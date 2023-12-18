5881da070f6112eb6a544299fa0fb7d3b23f7b3a
Borloo <supermaxime753@gmail.com>
2023-12-18 15:54:11 +0100

    Classes

--- Classes/Invocation.cs
+++ Classes/Invocation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +9,23 @@ namespace Invocators.Classes
 {
     internal class Invocation
     {
-        private int attack;
-        private int defense;
-        private int initiative;
-        private int damages;
-        private int maximumLife;
-        private int currentLife;
-        private int currentAttackNumber;
-        private int totalAttackNumber;
+        private int attack { get; set; }
+        private int defense { get; set; }
+        private int initiative { get; set; }
+        private int damages { get; set; }
+        private int maximumLife { get; set; }
+        private int currentLife { get; set; }
+        private int currentAttackNumber { get; set; }
+        private int totalAttackNumber { get; set; }
+        private int RoundInvocation { get; set; }
 
         public Invocation(
             int attack, 
             int defense, 
             int initiative, 
             int damages, 
-            int maximumLife, 
-            int currentLife, 
-            int currentAttackNumber, 
-            int totalAttackNumber
+            int maximumLife,
+            int roundInvocation
             )
         {
             this.attack = attack;
@@ -33,15 +33,55 @@ namespace Invocators.Classes
             this.initiative = initiative;
             this.damages = damages;
             this.maximumLife = maximumLife;
-            this.currentLife = currentLife;
-            this.currentAttackNumber = currentAttackNumber;
-            this.totalAttackNumber = totalAttackNumber;
+            this.currentLife = maximumLife;
+            this.currentAttackNumber = 2;
+            this.totalAttackNumber = 2;
+            this.RoundInvocation = roundInvocation;
         }
 
-        public int throwAttack()
+        public void Attack(Invocation targetInvocation)
         {
-            Random random = new Random();
-            return this.attack + random.Next(100);
+            if (this.currentAttackNumber > 0 && targetInvocation.currentLife > 0)
+            {
+                int attackRoll = this.CalculateAttackRoll();
+                int defenseRoll = this.CalculateDefenseRoll();
+                int attackDelta = attackRoll - defenseRoll;
+                if (attackDelta > 0)
+                {
+                    int damage = this.CalculateDamage(attackDelta);
+                    targetInvocation.EndureAttack(attackDelta);
+                    Console.WriteLine($"{GetType().Name} attack {targetInvocation.GetType().Name} and do damages {damage}");
+                }
+                else
+                {
+                    Console.WriteLine($"{GetType().Name} attaque {targetInvocation.GetType().Name} mais rate !");
+                }
+                this.currentAttackNumber--;
+            }
+        }
+
+        public void EndureAttack(int damages)
+        {
+            this.currentLife -= damages;
+            if (this.currentLife <= 0)
+            {
+                Console.WriteLine($"{GetType().Name}'s dead!");
+            }
+        }
+
+        private int CalculateAttackRoll()
+        {
+            return this.attack + new Random().Next(1, 101);
+        }
+
+        private int CalculateDefenseRoll()
+        {
+            return this.defense + new Random().Next(1, 101);
+        }
+
+        private int CalculateDamage(int attackDealte)
+        {
+            return (attackDealte * this.damages) / 100;
         }
     }
 }
---
+++ Classes/Invocator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invocators.Classes
+{
+    internal class Invocator
+    {
+        private int maximumLife { get; set; }
+        private int currentLife { get; set; }
+        private List<Invocation> invocations;
+
+        public Invocator(int maximumLife)
+        {
+            this.maximumLife = maximumLife;
+            this.currentLife = maximumLife;
+            this.invocations = new List<Invocation>();
+        }
+
+        public void Summon(Invocation invocation)
+        {
+            if (this.currentLife > 0)
+            {
+                this.invocations.Add(invocation);
+                Console.WriteLine("Succes summon");
+            }
+            else
+            {
+                Console.WriteLine("Not enough life");
+            }
+        }
+
+        public void GiveOrder(Invocation invocation, string order)
+        {
+            if (this.invocations.Contains(invocation))
+            {
+                Console.WriteLine($"Order give to {invocation.GetType().Name}: {order}");
+            }
+            else
+            {
+                Console.WriteLine("Invocation doens't exist in list of invocations");
+            }
+        }
+
+        public void ManageRoundFight()
+        {
+            foreach (var invocation in this.invocations)
+            {
+                Console.WriteLine($"{invocation.GetType().Name} attack!");
+                invocation.Attack(this.GetRandomTarget());
+            }
+        }
+
+        private Invocation GetRandomTarget()
+        {
+            return this.invocations[0];
+        }
+    }
+}
