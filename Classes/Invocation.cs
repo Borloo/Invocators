11cb37d34d34f637fb0b1bd2ef9469bda3fa735e
Borloo <supermaxime753@gmail.com>
2023-12-18 14:55:22 +0100

    Ajoutez des fichiers projet.

---
+++ Classes/Invocation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invocators.Classes
+{
+    internal class Invocation
+    {
+        private int attack;
+        private int defense;
+        private int initiative;
+        private int damages;
+        private int maximumLife;
+        private int currentLife;
+        private int currentAttackNumber;
+        private int totalAttackNumber;
+
+        public Invocation(
+            int attack, 
+            int defense, 
+            int initiative, 
+            int damages, 
+            int maximumLife, 
+            int currentLife, 
+            int currentAttackNumber, 
+            int totalAttackNumber
+            )
+        {
+            this.attack = attack;
+            this.defense = defense;
+            this.initiative = initiative;
+            this.damages = damages;
+            this.maximumLife = maximumLife;
+            this.currentLife = currentLife;
+            this.currentAttackNumber = currentAttackNumber;
+            this.totalAttackNumber = totalAttackNumber;
+        }
+
+        public int throwAttack()
+        {
+            Random random = new Random();
+            return this.attack + random.Next(100);
+        }
+    }
+}
---
+++ Classes/Invocators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invocators.Classes
+{
+    internal class Invocators
+    {
+        private int maximumLife;
+        private int currentLife;
+
+        public Invocators(int maximumLife)
+        {
+            this.maximumLife = maximumLife;
+            this.currentLife = maximumLife;
+        }
+    }
+}
